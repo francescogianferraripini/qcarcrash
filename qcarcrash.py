1a00093d8d3ed5efc4189a8e753351d2d447c9ca
Francesco Gianferrari Pini <francesco.gianferraripini@quantyca.it>
2018-11-06 10:13:13 +0100

    First real commit

--- qcarcrash.py
+++ qcarcrash.py
@@ -27,11 +27,74 @@ app = Starlette()
 
 cat_images_path = Path("/tmp")
 
+
+model1 = {
+    'name':'isDamaged',
+    'categories':['00-damage', '01-whole'],
+    'weights':'data1a-frozen10epochs.pth',
+    'modelType':models.resnet50,
+    'imageSize':299,
+    'transforms':get_transforms()
+}
+
+model2 = {
+    'name':'damageLocation',
+    'categories':['00-front', '01-rear', '02-side'],
+    'weights':'data2a-frozen10epochs-unfrozen10epochs.pth',
+    'modelType':models.resnet50,
+    'imageSize':299,
+    'transforms':get_transforms()
+}
+
+model3 = {
+    'name':'damageSeverity',
+    'categories':['01-minor', '02-moderate', '03-severe'],
+    'weights':'data3a-frozen10epochs-unfrozen10epochs.pth',
+    'modelType':models.resnet50,
+    'imageSize':299,
+    'transforms':get_transforms()
+}
+
+qcrashModelDefs = [model1,model2,model3]
+
+def generateLearner(modelDefition:dict, imagesPath:Path = Path("/tmp")):
+    
+    fnames = [
+        "/{}_1.jpg".format(c)
+        for c in modelDefition['categories']
+        ]
+    db=ImageDataBunch.from_name_re(
+        imagesPath,
+        fnames,
+        r"/([^/]+)_\d+.jpg$",
+        ds_tfms=modelDefition['transforms'],
+        size=modelDefition['imageSize'],
+    ).normalize(imagenet_stats)
+    learner = create_cnn(db, modelDefition['modelType'])
+    learner.model.load_state_dict(
+        torch.load(modelDefition['weights'], map_location="cpu")
+    )
+    return learner
+
+qcrashLearners = [generateLearner(md) for md in qcrashModelDefs]
+
+
+
+""" #Model Categories
 cat_fnames_l1 = [
     "/{}_1.jpg".format(c)
     for c in ['00-damage', '01-whole']
     ]
 
+cat_fnames_l2 = [
+    "/{}_1.jpg".format(c)
+    for c in ['00-front', '01-rear', '02-side']
+    ]
+
+cat_fnames_l3 = [
+    "/{}_1.jpg".format(c)
+    for c in ['01-minor', '02-moderate', '03-severe']
+    ]    
 
 
 cat_data_l1 = ImageDataBunch.from_name_re(
@@ -44,7 +107,7 @@ cat_data_l1 = ImageDataBunch.from_name_re(
 cat_learner_l1 = create_cnn(cat_data_l1, models.resnet50)
 cat_learner_l1.model.load_state_dict(
     torch.load("stage-1-50.pth", map_location="cpu")
-)
+) """
 
 
 @app.route("/upload", methods=["POST"])
@@ -62,7 +125,7 @@ async def classify_url(request):
 
 def predict_image_from_bytes(bytes):
     img = open_image(BytesIO(bytes))
-    pred_class,pred_idx,outputs = cat_learner.predict(img)
+    pred_class,pred_idx,outputs = qcrashLearners[0].predict(img)
     return JSONResponse({
         "predicted_class": pred_class
     })
@@ -93,3 +156,5 @@ def redirect_to_homepage(request):
 if __name__ == "__main__":
     if "serve" in sys.argv:
         uvicorn.run(app, host="0.0.0.0", port=8008)
+
+
